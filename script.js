bbc2d2df999dd3be275d362275084336afdd3fe7
almero0922 <albertomedel29@gmail.com>
2023-09-11 23:19:03 -0500

    Mobile Version Done with active states and script

---
+++ script.js
@@ -0,0 +1,16 @@
+const ca = document.getElementById('ca');
+const idE = document.getElementById('id');
+const textE = document.getElementById('text');
+ca.addEventListener('click', () => {
+    restApiCall();
+})
+
+function restApiCall(){
+    fetch(' https://api.adviceslip.com/advice' ,  { cache: "no-cache" })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        idE.innerText = "ADVICE #" + data.slip.id;
+        textE.innerText = '"'+data.slip.advice+'"';
+    })
+}
